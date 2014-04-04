cf70525808486e8d3ac5d4c5ef98e88181d69f76
Thomas Fini Hansen <xen@xen.dk>
2014-04-05 00:31:56 +0200

    More basic testing.

--- tests/bandaidTest.php
+++ tests/bandaidTest.php
@@ -17,6 +17,9 @@ class DeployotronCase extends Drush_CommandTestCase {
     // Copy in the command file, so the sandbox can find it.
     $drush_dir = getenv('HOME') . '/.drush';
     exec('cp -r ' . escapeshellarg(dirname(dirname(__FILE__))) . ' ' . escapeshellarg($drush_dir));
+    // Need to set up git minimally for it to work (else it wont commit).
+    exec('git config --global user.email "drush@example.com"');
+    exec('git config --global user.name "Bandaid Test cases"');
   }
 
   /**
@@ -37,13 +40,29 @@ class DeployotronCase extends Drush_CommandTestCase {
     $workdir = $this->webroot() . '/sites/all/modules';
     $this->drush('dl', array('panels-3.3'), array(), NULL, $workdir);
 
+    // Apply first patch, and check for success.
     $options = array(
       'home' => 'https://drupal.org/node/1985980',
       'reason' => 'For altering of new panes.',
     );
-    $this->assertEmpty($this->grep('drupal_alter(\'panels_new_pane\', $pane);', $workdir . '/panels'));
+    $patch1_string = 'drupal_alter(\'panels_new_pane\', $pane);';
+    $this->assertEmpty($this->grep($patch1_string, $workdir . '/panels'));
     $this->drush('bandaid-patch', array('https://drupal.org/files/issues/panels-new-pane-alter-1985980-5.patch', 'panels'), $options, NULL, $workdir);
-    $this->assertNotEmpty($this->grep('drupal_alter(\'panels_new_pane\', $pane);', $workdir . '/panels'));
+    $this->assertNotEmpty($this->grep($patch1_string, $workdir . '/panels'));
+
+    $options = array(
+      'home' => 'https://drupal.org/node/2098515',
+      'reason' => 'To avoid notice.',
+    );
+    $patch2_string = 'if (!isset($pane->type)) {';
+    $this->assertEmpty($this->grep($patch2_string, $workdir . '/panels'));
+    $this->drush('bandaid-patch', array('https://drupal.org/files/issues/undefined_property_notices_fix-2098515-2.patch', 'panels'), $options, NULL, $workdir);
+    $this->assertNotEmpty($this->grep($patch2_string, $workdir . '/panels'));
+
+    // Tearoff the patches and check that they're gone.
+    $this->drush('bandaid-tearoff', array('panels'), array(), NULL, $workdir);
+    $this->assertEmpty($this->grep($patch1_string, $workdir . '/panels'));
+    $this->assertEmpty($this->grep($patch2_string, $workdir . '/panels'));
 
   }
 
